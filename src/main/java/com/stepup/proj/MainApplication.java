f47ab38034737512dabe16f0b8fe2b1650f11629
Dmitrii Nasedkin <2_tone@mail.ru>
2024-06-27 23:09:09 +0300

    Улучшили логику

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,11 +4,12 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("1001");
+        System.out.println("----");
     }
 
     public static void doSomething(String[] args) {
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(0);
+        System.out.println(0);
     }
 }
