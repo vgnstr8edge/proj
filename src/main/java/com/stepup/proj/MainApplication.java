3709448a3395933b22399a72c7c76634cbfd4810
Dmitrii Nasedkin <2_tone@mail.ru>
2024-06-27 23:31:20 +0300

    добавил еще один doSomething

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -5,11 +5,13 @@ public class MainApplication {
         System.out.println("----");
         System.out.println("Hello World!");
         System.out.println("1001");
+        doSomething();
         System.out.println("----");
     }
 
+
     public static void doSomething(String[] args) {
-        System.out.println('hello');
+        System.out.println("hello");
 
     }
 }
