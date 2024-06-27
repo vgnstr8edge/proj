3fbab7f60662c635c4fabec99ec0d2f2ec42f8a3
Dmitrii Nasedkin <2_tone@mail.ru>
2024-06-27 23:12:47 +0300

    Изменили логику

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,13 +2,14 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("----");
         System.out.println("Hello World!");
         System.out.println("1001");
     }
 
     public static void doSomething(String[] args) {
+        System.out.println(0);
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(1);
     }
 }
